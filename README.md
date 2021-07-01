7a077955146b0faa0c32fb7c794b0bd78c1eaf55
Reto Trappitsch <reto@brandeis.edu>
2021-07-01 18:24:00 -0400

    First version of the GUI... Only GUI at the moment

--- README.md
+++ README.md
@@ -1,1 +1,207 @@
 # RPyScope
+
+This project is the start for a microscope interface
+using a Raspberry Pi Camera.
+The software interface is written in Python,
+thus the name `RPyScope`.
+
+## Installation
+
+We assume that you have the following components available:
+ - A raspberry pi with a camera that is enabled
+ - A stock installation of Raspberry Pi OS
+
+If you are not sure if your camera is enabled, 
+try and take an image according to the getting started guide
+[here](https://static.raspberrypi.org/files/product-guides/Raspberry_Pi_High_Quality_Camera_Getting_Started.pdf).
+
+### Installing Linux Pre-Requisites
+
+First make sure you have an updated operating system
+by running:
+
+```bash
+sudo apt update
+sudo apt upgrade
+```
+
+Next install the required packages as following:
+
+```bash
+sudo apt install python3-pyqt5
+```
+
+(See here: https://gist.github.com/malachib/0477b4280bfad85130d42f8f60ef38cd)
+
+### Clone Repository
+
+Next up,
+clone the repository from GitHub
+or download and unpack it.  
+**Note**: The repository is
+currently private,
+therefore you have to authenticate
+when cloning the repository.
+
+Cloning can be done in the folder of your interest
+by typing:
+```bash
+git clone https://github.com/galactic-forensics/RPyScope.git
+```
+for access via `https`, or 
+```bash
+git clone git@github.com:galactic-forensics/RPyScope.git
+```
+for access via `ssh` authentication.
+
+Now you should have a new folder named `RPyScope`
+wherever you cloned the repository into.
+
+### Running the RPyScope Software
+
+To run the software,
+go into the folder `RPyScope/rpyscope`
+and run the command:
+```bash
+python3 gui_qt.py
+```
+You should see the software start up. Give it a try!
+
+### Updates
+
+If you cloned the GitHub repository,
+you can simply go into the folder and pull from the repo
+in order to update your branch to the latest one.
+
+
+## Usage
+
+Many tool tips have been implemented. 
+Hover over something you want to adjust and read the tool tip.
+Here, only the functions that are not completely self-explanatory
+are discussed.
+
+### Exposure control
+
+Using the sliders,
+you can live-adjust brightness and contrast of the camera.
+Generally,
+the camera is set up such that 
+automatic expsosure is turned on (checked).
+For details, see
+[here](https://picamera.readthedocs.io/en/release-1.13/fov.html)
+on what that entails.
+
+When you turn automatic exposure off, 
+the camera will not regulate the shutter speed, 
+or digital and analog gain anymore.
+This is intended for taking images under stable conditions
+and follows the description in
+[Section 3.5](https://picamera.readthedocs.io/en/release-1.13/recipes1.html#capturing-consistent-images)
+in the PiCamera Manual.
+You can still adjust brightness and contrast.
+
+### Recording to files
+
+The recording path can be set by 
+clicking the button 
+`Set Recording Path`.
+This will specify which folder new files get saved to.
+Putting something into the file name input box
+will furthermore choose this filename.
+*Note* that currently,
+a full timestamp will be attached to the beginning of 
+each file name.
+
+By default,
+the path that you will record to is the home path,
+e.g., `/home/pi`.
+
+### Preview
+
+The raspberry pi camera preview can be started or stopped
+with the `Start Preview` / `Stop Preview` button.
+This preview directly streams to the GPU,
+therefore you are advised to keep the program on the very left
+or very right of the screen.
+If you don't move your mouse away from the button,
+you can always click it in case you cannot see it anymore
+after the preview starts.
+
+### Recording Video
+
+You can record videos by clicking the `Start Recording` button.
+The video recorded will be around as long as you set in the
+recording time input field. 
+Increments of 0.1 milliseconds are currently considered.
+You can always stop the recording early by pressing
+the `Stop Recording` button.
+
+If you leave the recording time at 0,
+video will be recorded until 
+you press the `Stop Recording` button.
+
+The video format that is outputted is currently fixed
+to `.h264`.
+
+**Limitation:** While images can be captured 
+during recordings,
+this functionality is currently not implemented.
+
+
+### Capturing Images
+
+If you want to take a photo,
+press the `Capture Image` button
+and the software will save an image in the 
+`.jpeg` format in the location of your choice. 
+Other formats are currently not supported,
+but will be in the future. 
+You can also easily change the default 
+by hacking around in the code :)
+
+### Command line interface (CLI)
+
+The second window is the command line interface.
+Many features are not currently implemented, 
+however, 
+if you can work with the `picamera` class,
+you can execute commands here directly from the software.
+The camera itself is stored into an instance called `cam`.
+So if you, e.g.,
+want to set the `iso` of the camera to fixed,
+you can type:
+
+```python
+camera.iso = 100
+```
+
+You can also use regular python commands here,
+they will all be interpreted.
+For example, typing:
+
+```python
+print(camera.iso)
+```
+
+Should print the current `iso` setting out to the terminal.
+If python raises an error,
+a messagebox will be displayed.
+
+**Important:**
+You can run any python command you want.
+However, 
+you can also interfere with the software itself.
+The idea of the CLI is to give the user an easy way
+of modifying stuff with commands and to play / explore the settings.
+If you need stuff over and over again, 
+it might be easier to implement it into the software.
+Also: you might be able to do real damage with the CLI to all your data,
+because you have the full power of python at your fingertips.
+Enjoy!
+
+## Issues, Problems, Questions...
+
+For issues, problems, questions,
+contributions, feature requests,
+please raise an Issue on GitHub.
---
+++ rpyscope/gui_qt.py
@@ -0,0 +1,353 @@
+"""GUI for RPyMicroscope."""
+
+import time
+import sys
+
+from PyQt5.QtWidgets import (
+    QWidget,
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QCheckBox,
+    QMessageBox,
+    QLabel,
+    QLineEdit,
+    QSlider,
+    QHBoxLayout,
+    QVBoxLayout,
+    QFileDialog,
+    QFrame,
+)
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QFont, QDoubleValidator
+
+
+class MainWindowControls(QMainWindow):
+    """Main Window with adjustments, etc. for Microscope GUI"""
+
+    def __init__(self):
+        # info variables
+        self.version = "0.0.1"
+        self.author = "Reto Trappitsch"
+
+        # init and sizing
+        super().__init__()
+        left = 0
+        top = 75
+        width = 180
+        height = 500
+        self.setGeometry(left, top, width, height)
+        self.setWindowTitle("RPyScope")
+
+        # colors
+        self.col_green = "#DBFFD4"
+        self.col_red = "#FFB6B6"
+
+        # recording timer
+        self.rec_timer_interval = 100  # interval of timing in msec
+        self.rec_time_elapsed = 0
+        self.rec_timer = QTimer()
+        self.rec_timer.setInterval(self.rec_timer_interval)
+        self.rec_timer.timeout.connect(self.recording_timer_check)
+
+        # main widget
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
+
+        # layout
+        layout = QVBoxLayout()  # main vbox layout
+        main_widget.setLayout(layout)
+
+        # Brightness & Contrast Labels
+        blabel = QLabel("Brightness")
+        font_center_bold(blabel)
+        clabel = QLabel("Contrast")
+        font_center_bold(clabel)
+        layout.addLayout(layout_horizontal([blabel, clabel]))
+
+        # Sliders
+        self.bright_slider = QSlider(Qt.Vertical)
+        self.contr_slider = QSlider(Qt.Vertical)
+        self.setup_sliders()
+        layout.addLayout(layout_horizontal([self.bright_slider, self.contr_slider]))
+        self.reset_bright_contr_button = QPushButton("Reset B/C")
+        self.reset_bright_contr_button.clicked.connect(self.reset_bright_contr)
+        self.reset_bright_contr_button.setToolTip("Reset brightness and contrast.")
+        layout.addWidget(self.reset_bright_contr_button)
+
+        # automatic exposure
+        lbl = QLabel("Auto exposure:")
+        self.auto_exp_checkbox = QCheckBox()
+        self.auto_exp_checkbox.setChecked(True)
+        self.auto_exp_checkbox.toggled.connect(self.set_auto_exposure)
+        self.auto_exp_checkbox.setToolTip(
+            "Enable or disable automatic exposure.\n"
+            "See Section 3.5 in PiCamera documentation."
+        )
+        layout.addLayout(layout_horizontal([lbl, self.auto_exp_checkbox]))
+
+        layout_hline(layout)
+
+        # file management
+        self.path_button = QPushButton("Set Recording Path")
+        self.path_button.clicked.connect(self.select_path)
+        self.path_button.acceptDrops()
+        self.path_button.setToolTip(
+            "Select the path where images and videos\n" "will be stored."
+        )
+        layout.addWidget(self.path_button)
+        layout.addWidget(QLabel("File name:"))
+        self.fname_input = QLineEdit()
+        self.fname_input.setToolTip(
+            "Select a filename. This will be appended\n" "to a time and date stamp."
+        )
+        layout.addWidget(self.fname_input)
+
+        layout_hline(layout)
+
+        # preview
+
+        self.preview_button = QPushButton("Start Preview")
+        self.preview_button.clicked.connect(self.preview_cam)
+        self.is_preview = False
+        self.preview_button.setStyleSheet(f"background-color:{self.col_green}")
+        self.preview_button.setToolTip(
+            "Start / Stop the video preview. The preview\n"
+            "is directly managed by the RPi and is sent\n"
+            "directly to your GPU. You might not see this\n"
+            "program anymore, which could be an issue."
+        )
+        layout.addWidget(self.preview_button)
+
+        layout_hline(layout)
+
+        # video recording
+
+        layout.addWidget(QLabel("Recording Time (s):"))
+        self.rec_time = QLineEdit()
+        self.rec_time.setValidator(QDoubleValidator(bottom=0))
+        self.rec_time.setAlignment(Qt.AlignRight)
+        self.rec_time.setText("0")
+        self.rec_time.setToolTip(
+            "Video recording time in seconds. If set\n"
+            "to 0, will record until button is pressed again."
+        )
+        layout.addWidget(self.rec_time)
+        self.rec_button = QPushButton("Start Recording")
+        self.rec_button.clicked.connect(self.record_video)
+        self.rec_button.setStyleSheet(f"background-color:{self.col_green}")
+        self.rec_button.setToolTip(
+            "Record a video. Click to start and stop\n" "or set a timer (see above)."
+        )
+        self.is_recording = False
+        layout.addWidget(self.rec_button)
+
+        layout_hline(layout)
+
+        # image recording
+        self.capture_button = QPushButton("Capture Image")
+        self.capture_button.clicked.connect(self.capture_image)
+        self.capture_button.setToolTip(
+            "Capture an image. Can currently only\n" "be done if video is not recorded."
+        )
+        layout.addWidget(self.capture_button)
+
+        # open command line interface
+        cli = CommandLineScope(parent=self, top=top + height + 50)
+        cli.show()
+
+    def setup_sliders(self):
+        """Setup for the sliders and all connects, etc."""
+        # brightness slider
+        self.bright_slider.setMinimum(0)
+        self.bright_slider.setMaximum(100)
+        self.bright_slider.setValue(50)
+        self.bright_slider.valueChanged.connect(lambda x: self.brightness_changed(x))
+
+        # contrast slider
+        self.contr_slider.setMinimum(-100)
+        self.contr_slider.setMaximum(100)
+        self.contr_slider.setValue(0)
+        self.contr_slider.valueChanged.connect(lambda x: self.contrast_changed(x))
+
+    # FUNCTIONS #
+
+    def brightness_changed(self, val):
+        """Change brightness to value"""
+        # todo
+        print(val)
+
+    def capture_image(self):
+        """Capture an image."""
+        pass
+        # todo capture an image
+
+    def contrast_changed(self, val):
+        """Change brightness to value"""
+        # todo
+        print(val)
+
+    def preview_cam(self):
+        """Preview camera."""
+        if not self.is_preview:  # not preview
+            self.preview_button.setText("Stop Preview")
+            self.preview_button.setStyleSheet(f"background-color:{self.col_red}")
+            # todo action
+            self.is_preview = True
+        else:
+            self.preview_button.setText("Start Preview")
+            self.preview_button.setStyleSheet(f"background-color:{self.col_green}")
+            # todo action
+            self.is_preview = False
+
+    def record_video(self):
+        """Start and stop recording."""
+        if not self.is_recording:  # not recording
+            self.rec_button.setText("Stop Recording")
+            self.rec_button.setStyleSheet(f"background-color:{self.col_red}")
+            self.capture_button.setDisabled(True)
+
+            # todo action to start recording
+
+            if float(self.rec_time.text()) > 0:
+                self.rec_timer.start()
+
+            self.is_recording = True
+        else:
+            self.rec_button.setText("Start Recording")
+            self.rec_button.setStyleSheet(f"background-color:{self.col_green}")
+            self.capture_button.setEnabled(True)
+
+            # todo action to stop recording
+
+            self.rec_timer.stop()
+            self.rec_time_elapsed = 0.0  # reset elapsed time
+            self.is_recording = False
+
+    def recording_timer_check(self):
+        """Check and stop the recording if elapsed time larger than total time."""
+        self.rec_time_elapsed += self.rec_timer_interval / 1000  # add to elapsed time
+        if self.rec_time_elapsed >= float(self.rec_time.text()):
+            # click record video to stop it, since it is started right now...
+            self.record_video()
+
+    def reset_bright_contr(self):
+        """Reset brightness and contrast slider."""
+        self.bright_slider.setValue(50)
+        self.contr_slider.setValue(0)
+
+    def select_path(self):
+        """Set the recording path via QFileDialogue."""
+        path = QFileDialog.getExistingDirectory(self, "Select Directory", "~")
+        print(path)
+        # todo set path to class
+
+    def set_auto_exposure(self):
+        """Turns automatic exposure of the camera on and off."""
+        if self.auto_exp_checkbox.isChecked():
+            # todo action
+            print("auto_exp on")
+            self.rec_timer.start()
+        else:
+            print("auto_exp off")
+
+
+class CommandLineScope(QMainWindow):
+    """Command line interface for camera, which must be a PiCamera Interface."""
+
+    def __init__(self, parent=None, top=0, cam=None):
+        """Initialize the CLI.
+
+        :param parent: parent class, defaults to None
+        :type parent: Class
+        :param top: Top adjuster, where to start, in px, defaults to 0
+        :type top: int
+        """
+        super(CommandLineScope, self).__init__(parent)
+
+        self.parent = parent
+        self.cam = cam
+
+        left = 0
+        width = 800
+        height = 0
+        self.setGeometry(left, top, width, height)
+        self.setWindowTitle("PiCamera CLI")
+
+        self.cli_edit = QLineEdit()
+        self.cli_edit.returnPressed.connect(self.cli_return_pressed)
+        self.cli_edit.setToolTip(
+            "Command Line Interface for camera settings.\n"
+            "Please refer to PiCamera documentation. Here,\n"
+            "if `cam = PiCamera()`, then you would enter\n"
+            "the command you would attach to `cam.`.\n\n"
+            "For example, if you would like to change `iso`,\n"
+            "type `iso = 100` and the microscope software\n"
+            "will send the command `cam.iso = 100`."
+        )
+
+        self.setCentralWidget(self.cli_edit)
+
+        if cam is None:
+            print("No camera defined!")
+
+    def cli_return_pressed(self):
+        """Return pressed in CLI."""
+        # get command and clear
+        cmd = self.cli_edit.text()
+        self.cli_edit.clear()
+
+        try:
+            eval(f"self.parent.{cmd}")
+        except Exception as e:
+            QMessageBox.critical(self, "Error", str(e), QMessageBox.Ok)
+
+
+#  HELPER FUNCTIONS #
+
+
+def font_center_bold(lbl):
+    """Sets a QLabel Centered and bold.
+
+    :param lbl: Label
+    :type lbl: QLabel
+
+    :return: None
+    """
+    font = QFont()
+    font.setBold(True)
+    lbl.setAlignment(Qt.AlignCenter)
+    lbl.setFont(font)
+
+
+def layout_hline(layout):
+    """Add a horizontal line to a layout."""
+    line = QFrame()
+    line.setFrameShape(QFrame.HLine)
+    line.setFrameShadow(QFrame.Sunken)
+    layout.addWidget(line)
+
+
+def layout_horizontal(items):
+    """Add items to QHBoxLayout and return the layout.
+
+    :param items: List of all Widgets
+    :type items: List(QtWidgets)
+
+    :return: Layout Hertically aligned
+    :rtype: Qt.QHBoxLayout
+    """
+    layout = QHBoxLayout()
+    layout.addStretch()
+    for item in items:
+        layout.addWidget(item)
+        layout.addStretch()
+    return layout
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindowControls()
+    window.show()
+
+    sys.exit(app.exec_())
