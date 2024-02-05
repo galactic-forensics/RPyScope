808662d2f686a999a4fc664f5d74ff6c9d847952
Reto Trappitsch <reto@galactic-forensics.space>
2024-02-05 16:07:16 +0100

    add way to run legacy mode from rye

--- src/rpyscope/__init__.py
+++ src/rpyscope/__init__.py
@@ -9,8 +9,18 @@ from rpyscope.gui_qt import MainWindowControls
 
 
 def demo():
+    """Run the app with the demo camera."""
     app = QtWidgets.QApplication(sys.argv)
     rpyscope_app = MainWindowControls(Cam.Demo)
+    rpyscope_app.show()
+
+    sys.exit(app.exec_())
+
+
+def rpihq_legacy():
+    """Run the app with the legacy support for the RPi HQ."""
+    app = QtWidgets.QApplication(sys.argv)
+    rpyscope_app = MainWindowControls(Cam.RPi_HQ)
     rpyscope_app.show()
 
     sys.exit(app.exec_())
